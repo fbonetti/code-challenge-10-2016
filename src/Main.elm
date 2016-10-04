6c3bb668f1b6a8656ab0d6c96924e1fd51be3f05
Frank Bonetti <frank.r.bonetti@gmail.com>
2016-10-03 20:26:19 -0500

    Add interactive parser

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,38 @@
 module Main exposing (..)
 
-import Html exposing (Html, Attribute, button, div, text, h2, p, br, pre, code)
-import Html.Attributes exposing (style)
+import Html exposing (Html, Attribute, button, div, text, h2, p, br, pre, code, textarea)
+import Html.Attributes exposing (style, value, rows)
+import Html.Events exposing (onClick, onInput)
 import String
 import Firewall
+import Html.App as App
 
 
-main : Html msg
+main : Program Never
 main =
-  div [ bodyStyle ]
-    [ h2 [] [ text "Input:" ]
-    , pre []
-        [ code [] [ text input ]
-        ]
-    , h2 [] [ text "Output:" ]
-    , pre []
-        [ code [] [ (Firewall.parseLines >> text) input ]
-        ]
-    ]
+  App.beginnerProgram
+    { model = initModel
+    , update = update
+    , view = view
+    }
 
-bodyStyle : Attribute msg
-bodyStyle =
-  style
-    [ ("margin", "0 auto")
-    , ("width", "600px")
-    , ("font-family", "Helvetica, Arial, sans-serif")
-    ]
+type alias Model =
+  { input : String
+  , tempInput : String
+  }
 
-input : String
-input =
+type Msg
+  = ParseInput
+  | SetTempInput String
+
+initModel : Model
+initModel =
+  { input = initInput
+  , tempInput = initInput
+  }
+
+initInput : String
+initInput =
   String.join "\n"
     [
       "10.0.0.1 1025 73.53.53.2 53",
@@ -41,4 +45,40 @@ input =
       "73.53.53.2 53 53.0.0.1 1025",
       "73.53.53.2 53 53.0.0.1 53",
       "73.53.53.1 53 53.0.0.1 1025"
+    ]
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    ParseInput ->
+      { model | input = model.tempInput }
+
+    SetTempInput tempInput ->
+      { model | tempInput = tempInput }
+
+view : Model -> Html Msg
+view { input, tempInput } =
+  div [ bodyStyle ]
+    [ h2 [] [ text "Input:" ]
+    , textarea [ inputTextAreaStyle, value tempInput, rows 10, onInput SetTempInput ] []
+    , button [ onClick ParseInput ] [ text "Parse" ]
+    , h2 [] [ text "Output:" ]
+    , pre []
+        [ code [] [ (Firewall.parseLines >> text) input ]
+        ]
+    ]
+
+bodyStyle : Attribute Msg
+bodyStyle =
+  style
+    [ ("margin", "0 auto")
+    , ("width", "600px")
+    , ("font-family", "Helvetica, Arial, sans-serif")
+    ]
+
+inputTextAreaStyle : Attribute Msg
+inputTextAreaStyle =
+  style
+    [ ("width", "100%")
+    , ("font-family", "monospace")
     ]
